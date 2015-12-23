885880a15a4d086901bcb22cb7fb03c4aa1add3c
piaoyaocdl <piaoyaocdl@163.com>
2015-12-23 20:59:39 +0800

    11111111111111111111

---
+++ src/main/java/com/hsw/RisApplication.java
@@ -0,0 +1,12 @@
+package com.hsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RisApplication.class, args);
+    }
+}
